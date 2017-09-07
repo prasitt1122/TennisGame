9085c87722f8673aff7b866ee5da610f0f107695
psps <game12597@gmail.com>
2017-09-07 18:41:33 +0700

    homework

--- tennis-game.spec.js
+++ tennis-game.spec.js
@@ -1,32 +1,76 @@
-function TennisGame(){
-  this.reset = ()=>{
+function TennisGame() {
+    const scoreString = ['Love', 'Fifteen', 'Thirty', 'Forthy']
+    this.playerAScore = 0
+    this.playerBScore = 0
 
-  }
+    this.reset = () => {
+        this.playerAScore = 0
+        this.playerBScore = 0
+    }
 
-  this.echo = ()=>{
+    this.echo = () => {
+       return `${scoreString[this.playerAScore]} - ${scoreString[this.playerBScore]}`
+    }
 
-
-
-  }
+    this.playerAGetScore = () => {
+        this.playerAScore++
+    }
+    this.playerBGetScore = () => {
+      this.playerBScore++
+    }
 }
 
+test('Echo "Love - Love" when game start', () => {
+    // Arrange
+    let app = new TennisGame()
 
-test('Echo "Love-Love" when press reset button',() =>{
+    // Act
+    app.reset()
+    let result = app.echo()
 
-  //Arrange เตรียมเพื่อทดสอบ
-  let app = new TennisGame()
+    // Assert
+    expect(result).toBe('Love - Love')
+})
 
+test('Echo "Fifteen - Love" when playerA get first score', () => {
+    // Arrange
+    let app = new TennisGame()
+    app.reset()
+    app.playerAGetScore()
 
-  //Act
-  app.reset()
-  let result = app.echo()
+    // Act
+    let result = app.echo()
 
+    // Assert
+    expect(result).toBe('Fifteen - Love')
+})
 
+test('Echo "Love - Fifteen" when playerB get first score', () => {
+    // Arrange
+    let app = new TennisGame()
+    app.reset()
+    app.playerBGetScore()
 
+    // Act
+    let result = app.echo()
 
+    // Assert
+    expect(result).toBe('Love - Fifteen')
+})
 
+test('Echo "Thirty - Forthy" When player A get 30 and player B get 40', () => {
+    // Arrange
+    let app = new TennisGame()
+    app.reset()
+    app.playerAGetScore()
+    app.playerAGetScore()
+    app.playerBGetScore()
+    app.playerBGetScore()
+    app.playerBGetScore()
 
-  //Assert คาดหวัง
-  expect(result).toBe('Love - Love')
+    // Act
+    let result = app.echo()
 
+    // Assert
+    expect(result).toBe('Thirty - Forthy')
 })
